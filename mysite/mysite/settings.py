4460a5eb2fba711845fa1be25f7a7486684ce96a
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-07-14 09:03:12 +0530

    updated code

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.staticfiles', # include middle ware to define the static file
 ]
 
 ROOT_URLCONF = 'mysite.urls'#
