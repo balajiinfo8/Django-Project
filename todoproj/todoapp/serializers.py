9ba5a585b00173d3111838921d27f9f10884b4a9
Balaji V <vinothkumarbalaji150@gmail.com>
2025-09-15 10:55:51 +0530

    Delete todoproj directory

--- todoproj/todoapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import todoapp
-
-class todoserializer(serializers.ModelSerializer):
-    class Meta:
-        fields = '__all__'
-        model = todoapp
