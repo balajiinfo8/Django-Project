99781be053425cfb66cd52691aec9e840ae742bc
Balaji V <vinothkumarbalaji150@gmail.com>
2025-09-15 10:55:13 +0530

    Delete TODO-LIST/todoproj directory

--- TODO-LIST/todoproj/todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TODOAPP(models.Model):
-    id = models.IntegerField(primary_key=True)
-    work_name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.work_name
--- TODO-LIST/todoproj/todoapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import TODOAPP
-from rest_framework import serializers
-
-class TODOSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TODOAPP
-        fields = '__all__'
